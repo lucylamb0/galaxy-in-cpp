eb280eca56143ae0e64443ed71969c8bc0e18164
Conni Bilham <me@conni.lgbt>
2022-10-28 11:59:06 +0100

    Tidy region and region matrix

--- Region.h
+++ Region.h
@@ -5,53 +5,24 @@
 
 #include <iostream>
 #include "Vector3.h"
+#include "Star.h"
+
+// TODO: Give each region a COM
+// TODO: If a region has no stars set a flag to not calculate it and to skip it when calculating star accels
 class Star;
+class CentreMass {
+public:
+    Vector position;
+    long double mass;
+};
 class Region {
 public:
     Vector min, max;
-    Vector com_position = Vector(0, 0, 0);
-    float com_mass = 0;
+
+    CentreMass centreMass;
 
     std::vector<Star*> stars_in_region = {};
     Region(Vector min, Vector max) : min(min), max(max) {}
-};
-// TODO: Give each region a COM
-// TODO: If a region has no stars set a flag to not calculate it and to skip it when calculating star accels
-
-#define RegionArrayT std::vector<Region*>
-class RegionMatrix {
-public:
-    // TODO: Fine tune a decent overlap factor - higher == more overlaps for stars == longer runtimes == higher == greater accuracy?
-    float overlap_factor;
-
-    RegionArrayT regions;
-    Vector step, overlap, divisions;
-
-    Vector simulationSpaceStart, simulationSpaceEnd = {};
-
-    RegionMatrix() = default;
-
-    RegionMatrix(Vector min, Vector max, Vector divisions, float overlap_factor = 0.0003f) :
-        simulationSpaceStart(min), simulationSpaceEnd(max), divisions(divisions), overlap_factor(overlap_factor)
-    {
-        step = (simulationSpaceEnd - simulationSpaceStart) / divisions;
-        overlap = Vector(step.x * overlap_factor, step.y * overlap_factor, step.z * overlap_factor); // overlap between regions
-
-        for (int i = 0; i < divisions.x; i++) {
-            for (int j = 0; j < divisions.y; j++) {
-                for (int k = 0; k < divisions.z; k++) {
-                    regions.push_back(new Region(Vector((simulationSpaceStart.x + i * step.x) - overlap.x,
-                                                        (simulationSpaceStart.y + j * step.y) - overlap.y,
-                                                        (simulationSpaceStart.z + k * step.z) - overlap.z
-                                                 ),
-                                                 Vector((simulationSpaceStart.x + (i + 1) * step.x) + overlap.x,
-                                                        (simulationSpaceStart.y + (j + 1) * step.y) + overlap.y,
-                                                        (simulationSpaceStart.z + (k + 1) * step.z) + overlap.z
-                                                 )));
-                }
-            }
-        }
-    }
 };
 
 #endif //C_VERSION_REGION_H
