3539f8c21da6a194497617466c6b827f5aae7fce
Conni Bilham <me@conni.lgbt>
2022-11-10 18:40:32 +0000

    Push a star unit test

---
+++ GTests/Star_Test.cc
@@ -0,0 +1,40 @@
+// Copyright (c) Conni Bilham & Lucy Coward 2022, All Rights Reserved.
+//
+//
+// Created by Conni Bilham on 10/11/2022.
+//
+
+#ifndef GALAXYSIMULATION_STAR_TEST_CC
+#define GALAXYSIMULATION_STAR_TEST_CC
+
+#include <gtest/gtest.h>
+#include "../Star.h"
+
+TEST(Star_Test, Create_Star_Test) {
+    Star* star = new Star(
+            1,
+            Vector(1, 2, 3),
+            Vector(4, 5, 6),
+            Vector(7, 8, 9),
+            10,
+            nullptr,
+            0
+            );
+
+    EXPECT_TRUE(star->id == 1);
+    EXPECT_TRUE(star->position.x == 1);
+    EXPECT_TRUE(star->position.y == 2);
+    EXPECT_TRUE(star->position.z == 3);
+    EXPECT_TRUE(star->velocity.x == 4);
+    EXPECT_TRUE(star->velocity.y == 5);
+    EXPECT_TRUE(star->velocity.z == 6);
+    EXPECT_TRUE(star->acceleration.x == 7);
+    EXPECT_TRUE(star->acceleration.y == 8);
+    EXPECT_TRUE(star->acceleration.z == 9);
+    EXPECT_TRUE(star->mass == 10);
+    EXPECT_TRUE(star->parent == nullptr);
+    EXPECT_FALSE(star->is_static());
+    EXPECT_TRUE(star->regions_we_are_in.size() == 0);
+}
+
+#endif //GALAXYSIMULATION_STAR_TEST_CC
